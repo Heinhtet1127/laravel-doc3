f23b6591c85d4a67a512a4b541f1e66e6f0c9611
Hein <rhovanion115@gmail.com>
2026-08-20 16:17:43 +0630

    docker mysql nginx

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('users', function () {
+    User::create([
+        "name" => "Test",
+        "email" => "test2@gmail.com",
+        "password" => "123456",
+    ]);
+
+    dd(User::get()->toArray());
 });
